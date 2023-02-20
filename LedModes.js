8ae0fc8ac5ff6884eedd26910ee3aba3855a23aa
Maxmilián Dao <maxmilian.dao@outlook.com>
2023-02-20 02:02:10 +0100

    Update LedModes.js

--- LedModes.js
+++ LedModes.js
@@ -1,7 +1,33 @@
+let state = 1
+
 input.onButtonPressed(Button.A, function () {
-    state += 0 - 1
+    state -= 1
     basic.clearScreen()
 })
+
+input.onButtonPressed(Button.B, function () {
+    state += 1
+    basic.clearScreen()
+})
+
+// all the possible LED states
+function leds_off() {
+    basic.forever(function () {}
+        )
+    }
+
+function leds_on() {
+    basic.forever(function () {
+        basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    })
+}
+
 function leds_flash () {
     basic.forever(function () {
         basic.showLeds(`
@@ -16,21 +42,7 @@ function leds_flash () {
         basic.pause(200)
     })
 }
-function leds_on () {
-    basic.forever(function () {
-        basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-    })
-}
-input.onButtonPressed(Button.B, function () {
-    state += 1
-    basic.clearScreen()
-})
+
 function leds_alternate () {
     basic.forever(function () {
         basic.showLeds(`
@@ -55,12 +67,7 @@ function leds_alternate () {
         basic.pause(50)
     })
 }
-// all the possible LED states
-function leds_off () {
-    basic.forever(function () { }
-    )
-}
-let state = 1
+
 // checks for and displays the desired LED mode, use buttons to pick
 basic.forever(function () {
     if (state > 4) {
